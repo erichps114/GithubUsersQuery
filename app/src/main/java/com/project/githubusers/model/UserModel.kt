b1c724fc6ed02b41dcfa050b18abf4e9d3b7f495
ErichPS <erichpratamasaputra@gmail.com>
2019-02-16 15:37:56 +0700

    Fix serializeable issue in data class

--- app/src/main/java/com/project/githubusers/model/UserModel.kt
+++ app/src/main/java/com/project/githubusers/model/UserModel.kt
@@ -1,6 +1,8 @@
 package com.project.githubusers.model
 
+import java.io.Serializable
+
 data class UserModel (
     var login : String? = "",
     var avatar_url : String?  = ""
-)
+) : Serializable
